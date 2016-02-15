113882e2650e79bb5449f644290f4ab6dc048c76
Makmesh iKiev <makmesh.x@gmail.com>
2016-02-15 13:05:04 +0300

    Fixed a bug in generating payroll

--- reports/payslip.php
+++ reports/payslip.php
@@ -209,7 +209,7 @@ foreach ($employees as $employee) {
     $pdf->Cell($w3, $h, $he, 0, 1, "R", true);
 
     $relief = $deductions[0]->value;
-    $totalDeduction += $relief;
+    $totalDeduction -= $relief;
     $pdf->Cell($w, $h, "MONTHLY RELIEF");
     $pdf->Cell($w2, $h, "");
     $pdf->Cell($w3, $h, "(" . number_format($relief, 2) . ")", 0, 1, "R", true);
